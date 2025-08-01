288ed1c366e6c9bb1eec6155fb108b402b00fa83
Wilhem Barbier <wilhem@wbrbr.org>
2025-08-01 19:34:05 +0200

    wip

--- scripts/setup_vulkan_sdk.sh
+++ scripts/setup_vulkan_sdk.sh
@@ -2,7 +2,7 @@
 
 sdk_version=$(curl https://vulkan.lunarg.com/sdk/latest/linux.txt)
 
-curl -O http://sdk.lunarg.com/sdk/download/latest/linux/vulkan_sdk.tar.xz
+curl -O https://sdk.lunarg.com/sdk/download/latest/linux/vulkan_sdk.tar.xz
 
 
 tar xf vulkan_sdk.tar.xz
